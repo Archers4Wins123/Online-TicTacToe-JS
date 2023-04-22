b54d0d0e0d1a1fea1b084ecb1bb3dc5c8083c66d
Wenpan <wenpan@Apples-MBP.hitronhub.home>
2023-04-22 13:06:37 -0600

    The app can connect with websocket server and receive game state properly.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useCallback, useEffect } from "react";
+import useWebSocket, { ReadyState } from 'react-use-websocket';
 
 function App() {
+  const URL = "ws://localhost:8080/play";
+  /*useEffect(() => {
+    const websocket = new WebSocket(URL)
+
+    websocket.onopen = () => {
+      console.log('connected');
+    }
+
+    websocket.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+    }
+  }, []);*/
+  const {sendMessage,
+    sendJsonMessage,
+    lastMessage,
+    lastJsonMessage,
+    readyState,
+    getWebSocket} = useWebSocket(URL, {
+      onOpen: () => console.log('opened'),
+      shouldReconnect: (closeEvent) => true,
+  });
+  const [messageHistory, setMessageHistory] = useState([]);
+  const handleClickSendMessage = useCallback(() => {
+    sendMessage('make_turn#{"x":0,"y":0}');
+    console.log("lastMessage="+lastMessage);
+  }, []);
+  const connectionStatus = {
+    [ReadyState.CONNECTING]: 'Connecting',
+    [ReadyState.OPEN]: 'Open',
+    [ReadyState.CLOSING]: 'Closing',
+    [ReadyState.CLOSED]: 'Closed',
+    [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
+  }[readyState];
+  const [gameState, setGameState] = useState({
+    playerAtTurn: 'X',
+    field: [["X","X",""],["X","X","O"],["X","X"," "]],
+    winningPlayer: null,
+    isBoardFull: false,
+    connectedPlayers: []
+  });
+  useEffect(()=> {
+    //getWebSocket().onmessage = console.log;
+    //getWebSocket().onclose = ()=> {
+    //  console.log("WebSocket socket closed.")
+    //};
+    if(lastMessage !== null) {
+      console.log("received =" + lastMessage);
+      setMessageHistory((prev) => prev.concat(lastMessage));
+      try {
+        console.log("parsed to " + JSON.parse(lastMessage));
+      } catch(e) {
+        console.log("parse failed");
+      }
+    }
+  }, [lastMessage, setMessageHistory]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <tbody>
+          {gameState.field.map((subArray, index)=>(
+            <tr>              
+              <td><button onClick={handleClickSendMessage} style={{width: '30px', height: '30px'}}>{subArray[0]}</button></td>
+              <td><button style={{width: '30px', height: '30px'}}>{subArray[1]}</button></td> 
+              <td><button style={{width: '30px', height: '30px'}}>{subArray[2]}</button></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div>readystate is {readyState}, {connectionStatus}</div>
+      <span>The WebSocket is currently {connectionStatus}</span>
+      {lastMessage ? <span>Last message: {lastMessage.data}</span> : null}
+      <ul>
+        {messageHistory.map((message, idx) => (
+          <span key={idx}>{message ? message.data : null}</span>
+        ))}
+      </ul>
     </div>
   );
 }
